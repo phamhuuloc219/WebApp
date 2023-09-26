24dc09005e78820ed0c67eb0b6a9dcdadf8526fb
phamhuuloc219 <94950295+phamhuuloc219@users.noreply.github.com>
2023-09-27 00:28:20 +0700

    WebAppN2
    PHLIT5

---
+++ WebAppIT5/WebAppIT5/Controllers/SiteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppIT5.Controllers
+{
+    public class SiteController : Controller
+    {
+        // GET: Site
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
