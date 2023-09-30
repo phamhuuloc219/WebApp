0e50660c4b6b1c2b2a69a5466a96d76cc6092d91
phamhuuloc219 <94950295+phamhuuloc219@users.noreply.github.com>
2023-10-01 00:00:41 +0700

    Tuần 2
    Thêm lớp MyClass và tạo CSDL vs framework entity

---
+++ WebAppIT5/MyClass/Model/Users.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClass.Model
+{
+    [Table("Users")]
+    public class Users
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Tên đăng nhập")]
+        public string Username { get; set; }
+        [Required]
+        [Display(Name = "Mật khẩu")]
+        public string Password { get; set; }
+        [Required]
+        [Display(Name = "Họ tên người dùng")]
+        public string Fullname { get; set; }
+        [Required]
+        [Display(Name = "Thư điện tử")]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "Số điện thoại")]
+        public string Phone { get; set; }
+        [Display(Name = "Hình")]
+        public string Img { get; set; }
+        [Required]
+        [Display(Name = "Giới tính")]
+        public string Gender { get; set; }
+        [Required]
+        [Display(Name = "Quyền truy cập")]
+        public string Role { get; set; }
+        [Display(Name = "Địa chỉ")]
+        public string Address { get; set; }
+        [Display(Name = "Ngày tạo")]
+        public DateTime? CreateAt { get; set; }
+        [Display(Name = "Người tạo")]
+        public int? CreateBy { get; set; }
+        [Display(Name = "Ngày cập nhật")]
+        public DateTime? UpdateAt { get; set; }
+        [Display(Name = "Người cập nhật")]
+        public int? UpdateBy { get; set; }
+        [Display(Name = "Trạng thái")]
+        public int? Status { get; set; }
+    }
+}
--- WebAppIT5/WebAppIT5/Controllers/SiteController.cs
+++ WebAppIT5/WebAppIT5/Controllers/SiteController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyClass.Model;
 
 namespace WebAppIT5.Controllers
 {
@@ -11,6 +12,9 @@ namespace WebAppIT5.Controllers
         // GET: Site
         public ActionResult Index()
         {
+            /*MyDBContext db = new MyDBContext();
+            int sodong = db.Products.Count();
+            ViewBag.sodong = sodong;*/
             return View();
         }
     }
