fa92c2cea81ea08ba46bb69d7e37933a1add143c
phamhuuloc219 <94950295+phamhuuloc219@users.noreply.github.com>
2023-10-07 21:59:23 +0700

    Ngày 07-10-2023

---
+++ WebAppIT5/MyClass/DAO/CategoriesDAO.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClass.Model;
+
+namespace MyClass.DAO
+{
+    public class CategoriesDAO
+    {
+        private MyDBContext db = new MyDBContext();
+        /// ////////////////////////////////////////////////////////////////////////////////////
+        /// Index
+        public List<Categories> getList()
+        {
+            return db.Categories.ToList();
+        }
+
+        /// ////////////////////////////////////////////////////////////////////////////////////
+        /// index voi gia tri Status 1,2- 0: an khoi trang giao dien
+        public List<Categories> getList(string status="ALL")
+        {
+            List<Categories> list = null;
+            switch (status)
+            {
+                case "Index": // status =1,2
+                    {
+                        list = db.Categories.Where(m => m.Status != 0).ToList();
+                        break;
+                    }
+                case "Trash": //status=0
+                    {
+                        list = db.Categories.Where(m => m.Status == 0).ToList();
+                        break;
+                    }
+                default:
+                    {
+                        list = db.Categories.ToList();
+                        break;
+                    }
+            }
+            return list;
+        }
+        /// ////////////////////////////////////////////////////////////////////////////////////
+        /// Details hien thi 1 dong du lieu
+        public Categories getRow(int? id)
+        {
+            if (id == null)
+            {
+                return null;
+            }
+            else
+            {
+                return db.Categories.Find(id);
+            }
+        }
+        /// ////////////////////////////////////////////////////////////////////////////////////
+        /// create = insert 1 dong database
+        public int Insert(Categories row)
+        {
+            db.Categories.Add(row);
+            return db.SaveChanges();
+        }
+        /// ////////////////////////////////////////////////////////////////////////////////////
+        /// edit= update 1 dong DB
+        public int Update(Categories row)
+        {
+            db.Entry(row).State = EntityState.Modified;
+            return db.SaveChanges();
+        }
+        /// ////////////////////////////////////////////////////////////////////////////////////
+        /// delete 1 dong DB
+        public int Delete(Categories row)
+        {
+            db.Categories.Remove(row);
+            return db.SaveChanges();
+        }
+    }
+}
--- WebAppIT5/MyClass/Model/Categories.cs
+++ WebAppIT5/MyClass/Model/Categories.cs
@@ -12,20 +12,32 @@ namespace MyClass.Model
     public class Categories
     {
         [Key]
+        [Display(Name="Id")]
         public int Id { get; set; }
         [Required]
+        [Display(Name = "Tên loại sản phẩm")]
         public string Name { get; set; }
+        [Display(Name = "Tên rút gọn")]
         public string Slug { get; set; }
+        [Display(Name = "Cấp cha")]
         public int? ParentID { get; set; }
+        [Display(Name = "Sắp xếp")]
         public int? Order { get; set; }
         [Required]
+        [Display(Name = "Mô tả")]
         public string MetaDesc { get; set; }
         [Required]
+        [Display(Name = "Từ khóa")]
         public string MetaKey { get; set; }
+        [Display(Name = "Ngày tạo")]
         public DateTime CreateAt { get; set; }
+        [Display(Name = "Người tạo")]
         public int CreateBy { get; set; }
+        [Display(Name = "Ngày cập nhật")]
         public DateTime UpdateAt { get; set; }
+        [Display(Name = "Người cập nhật")]
         public int UpdateBy { get; set; }
+        [Display(Name = "Trạng thái")]
         public int Status { get; set; }
     }
 }
